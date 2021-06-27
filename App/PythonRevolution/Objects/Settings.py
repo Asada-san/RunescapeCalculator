c5cd211e098d8adf7307ebe9aedba1f767f87806
Asada-san <ploxmicky@live.nl>
2021-06-27 20:23:09 +0200

    V1.06.0

--- App/PythonRevolution/Objects/Settings.py
+++ App/PythonRevolution/Objects/Settings.py
@@ -6,8 +6,8 @@ class Settings:
     def __init__(self, userInput):
 
         self.run = True  # True if we want to keep the while loop running
-        # self.Debug = userInput['Debug']  # When True, lots of info is printed to the HTML file
-        self.Debug = True
+        self.Debug = userInput['Debug']  # When True, lots of info is printed to the HTML file
+        # self.Debug = True
 
         # Check user input for a possible value for simulation time and starting adrenaline
         if userInput['simulationTime'] != '' and userInput['Adrenaline'] != '' and \
